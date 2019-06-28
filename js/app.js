c0b31751b9c0f9b2f82ad6e5adcca03dd3e412a3
Nenino2 <nennino2@libero.it>
2019-06-28 12:03:56 +0200

    Added user interface mechanics

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
-const text_input = "Messaggio di testo";
+// SETTINGS
 const default_alphabet = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz'.split(''));
-const key_input = "";
-const key = key_input.split('').splice(0, 3);
-const r1_key = key[0];
-const r2_key = key[1];
-const r3_key = key[2];
+
+// INTERFACE
+
+const main_page = document.querySelector('.main-page');
+const key_page = document.querySelector('.key-page');
+const results_page = document.querySelector('.results-page');
+
+const message_textarea = document.getElementById('messageInput');
+const key_textarea = document.getElementById('keyInput');
+const output_textarea = document.getElementById('messageOutput');
+
+const encrypt_button = document.getElementById('btnEncrypt');
+const decrypt_button = document.getElementById('btnDecrypt');
+const submit_button = document.getElementById('btnSubmit');
+const received_button = document.getElementById('btnReceived');
 
 // ROTORS DEFINITION
 class Rotor {
@@ -132,45 +142,100 @@ class Rotor {
 
 // ENIGMA
 
-const enigma = {
-  r1: new Rotor(r1_key),
-  r2: new Rotor(r2_key, 1),
-  r3: new Rotor(r3_key, 2),
+class Enigma {
+  constructor(r1_key = 'A', r2_key = 'A', r3_key = 'A') {
+    this.r1 = new Rotor(r1_key);
+    this.r2 = new Rotor(r2_key, 1);
+    this.r3 = new Rotor(r3_key, 2);
+  }
 
   encrypt_letter(l) {
     const l1 = this.r1.encrypt_letter(l);
     const l2 = this.r2.encrypt_letter(l1);
     const l3 = this.r3.encrypt_letter(l2);
     return l3;
-  },
+  }
 
   decrypt_letter(l) {
     const l1 = this.r1.decrypt_letter(l);
     const l2 = this.r2.decrypt_letter(l1);
     const l3 = this.r3.decrypt_letter(l2);
     return l3;
-  },
+  }
 
   encrypt_words(text) {
-    textArray = text.split('');
+    const textArray = text.split('');
     const cryptedArray = textArray.map(letter => this.encrypt_letter(letter));
     return cryptedArray.join('');
-  },
+  }
 
   decrypt_words(text) {
-    textArray = text.split('');
+    const textArray = text.split('');
     const cryptedArray = textArray.map(letter => this.decrypt_letter(letter));
     return cryptedArray.join('');
-  },
+  }
 
   reset() {
-    this.r1.set_key(r1_key);
-    this.r2.set_key(r2_key);
-    this.r3.set_key(r3_key);
+    this.r1.set_key('A');
+    this.r2.set_key('A');
+    this.r3.set_key('A');
   }
 }
 
-console.log(enigma.encrypt_words(text_input));
-console.log(enigma.encrypt_words(text_input));
-console.log(enigma.encrypt_words(text_input));
-console.log(enigma.encrypt_words(text_input));
+// APP
+
+
+let text_input;
+let key;
+let r1_key;
+let r2_key;
+let r3_key;
+let enigma;
+let output;
+let criptMode;
+
+encrypt_button.addEventListener('click', () => {
+  if (message_textarea.value) {
+    text_input = message_textarea.value;
+    main_page.style.display = 'none';
+    key_page.style.display = 'block';
+    message_textarea.value = '';
+    criptMode = 'Encrypt';
+  }
+});
+
+decrypt_button.addEventListener('click', () => {
+  if (message_textarea.value) {
+    text_input = message_textarea.value;
+    main_page.style.display = 'none';
+    key_page.style.display = 'block';
+    message_textarea.value = '';
+    criptMode = 'Decrypt';
+  }
+});
+
+submit_button.addEventListener('click', () => {
+  if (key_textarea.value) {
+     key = key_textarea.value.split('').splice(0, 3);
+     r1_key = key[0];
+     r2_key = key[1];
+     r3_key = key[2];
+     enigma = new Enigma(r1_key, r2_key, r3_key);
+     key_page.style.display = 'none';
+     key_textarea.value = '';
+     results_page.style.display = 'block';
+     if (criptMode === 'Encrypt') {
+       output_textarea.innerText = enigma.encrypt_words(text_input);
+     } else if (criptMode === 'Decrypt') {
+       output_textarea.innerText = enigma.decrypt_words(text_input);
+     } else {
+       output_textarea.innerText = 'Internal error';
+     }
+  }
+})
+
+received_button.addEventListener('click', () => {
+  results_page.style.display = 'none';
+  main_page.style.display = 'block';
+  enigma.reset();
+})
